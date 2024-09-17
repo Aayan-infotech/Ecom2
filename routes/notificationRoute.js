e44165126674f33eb023d70b14097bd14ae5f5b8
Nivedita <nivedita.upadhayay@aayaninfotech.com>
2024-09-17 13:09:14 +0530

    Current changes

--- routes/notificationRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getNotification, markAsRead, deleteNotifications } = require('../controllers/notificationController');
-
-router.get('/get', getNotification);
-router.put('/:notificationId/markAsRead', markAsRead);
-router.delete('/delete/:id', deleteNotifications);
-
-module.exports = router;
