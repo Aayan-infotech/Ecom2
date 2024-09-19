3d7f5f4a8a3424a3c31ac3ddecc7e99790a027f4
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2024-09-19 14:23:48 +0530

    add change notification

---
+++ controllers/notification.js
@@ -0,0 +1,55 @@
+const admin = require('firebase-admin');
+const serviceAccount =require('../firebase.json')
+const notificationModel=require('../models/notificationModel')
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+const notification = async (userId,title, body,deviceToken)=>{
+  try {
+    const message = {
+        notification: {
+          title: title,
+          body: body
+        },
+        token:deviceToken
+      };
+
+      if(userId){
+        const newUser = new notificationModel({
+            userId,
+            body,
+            title,
+        });
+        await newUser.save();
+      }
+      
+      await admin.messaging().send(message)
+      .then((response) => {
+        console.log('Successfully sent message:', response);
+      })
+      .catch((error) => {
+        console.log('Error sending message:', error);
+      });
+  } catch (e) {
+    return e.message
+  }
+};
+
+const getNotification=async(req,res)=>{
+  try{
+    const result=await notificationModel.find({userId:req.params.id})
+    res.json({
+      status:200,
+      msg:"Get all notification",
+      data:result
+    })
+  }catch(error){
+    res.status(500).json({
+      status: 500,
+      error: error.message
+    });
+  }
+}
+
+module.exports={notification,getNotification}
